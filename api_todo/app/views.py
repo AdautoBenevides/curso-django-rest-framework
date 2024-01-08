0ca00fb4dcb12289b6c80d4a208ffe50ea4fce4b
Adauto Benevides Couto <daucouto9@gmail.com>
2024-01-07 23:49:22 -0300

    commit alterações

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from app.models import Todo
+from app.serializers import TodoSerializer
 
-# Create your views here.
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    
